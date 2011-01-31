cfb27009694bdd6bd8ab00bddc737146fcac8a03
burger.gun@gmail.com <burger.gun@gmail.com@0a9feec2-0c5f-d26a-50eb-29f2d576f81e>
2011-01-31 22:12:25 +0000

    Corrected build scripts

--- trunk/windows-build/build_debug.bat
+++ trunk/windows-build/build_debug.bat
@@ -1,35 +1,43 @@
-rem BCC build for Steem
-rem make sure BCCROOT and PATH variables are set for your install of BCC
+rem These are the BCC (Borland C++ Compiler) build scripts
+rem for Steem (debug / developers' build).
+rem You need BCC from https://downloads.embarcadero.com/item/24778
+rem and NASM from http://www.nasm.us/
 
-rmdir /S /Q "_DebugRelease"
-mkdir "_DebugRelease"
+rem make sure BCCROOT environment variable is set to point to
+rem the bcc5.5 directory and the PATH variable points to
+rem bcc5.5\bin\make.exe and nasm.exe
+
+set OUT=_DebugRelease
+
+rmdir /S /Q %OUT%
+mkdir "%OUT%"
 
 rmdir /S /Q obj
 mkdir obj
 
-nasm -o obj\asm_draw.obj -fobj -dWIN32 -w+macro-params -w+macro-selfref -w+orphan-labels -i..\steem\asm ..\steem\asm\asm_draw.asm
-nasm -o obj\asm_osd.obj -fobj -dWIN32 -w+macro-params -w+macro-selfref -w+orphan-labels -i..\steem\asm ..\steem\asm\asm_osd_draw.asm
+nasm -o obj\asm_draw.obj -fobj -dWIN32 -w+macro-params -w+macro-selfref -w+orphan-labels -i..\steem\asm\ ..\steem\asm\asm_draw.asm
+nasm -o obj\asm_osd_draw.obj -fobj -dWIN32 -w+macro-params -w+macro-selfref -w+orphan-labels -i..\steem\asm\ ..\steem\asm\asm_osd_draw.asm
 nasm -o obj\asm_portio.obj -fobj -dWIN32 ..\include\asm\asm_portio.asm
 
-
 make.exe -B -D_FORCE_DEBUG_BUILD -D_BCC_BUILD
-del _DebugRelease\*.tds
 
-mkdir "_DebugRelease\patches"
-xcopy "..\steem\patches"             "_DebugRelease\patches\" /y /e
+del %OUT%\*.tds
+
+mkdir "%OUT%\patches"
+xcopy "..\steem\patches"              "%OUT%\patches\" /y /e
 
 set DOC_DIR=..\steem\doc
 
-copy "%DOC_DIR%\readme.txt"           "_DebugRelease\readme.txt"
-copy "..\steem\lib\unzipd32.dll"         "_DebugRelease\unzipd32.dll"
-copy "..\steem\steem.new"            "_DebugRelease\steem.new"
-copy "%DOC_DIR%\DISK_IMG.PRG"         "_DebugRelease\DISK_IMG.PRG"
-copy "%DOC_DIR%\disk image howto.txt" "_DebugRelease\disk image howto.txt"
-copy "%DOC_DIR%\CART_IMG.PRG"         "_DebugRelease\CART_IMG.PRG"
-copy "%DOC_DIR%\cart image howto.txt" "_DebugRelease\cart image howto.txt"
-copy "%DOC_DIR%\faq.txt"              "_DebugRelease\faq.txt"
+copy "%DOC_DIR%\readme.txt"           "%OUT%\readme.txt"
+copy "..\steem\lib\unzipd32.dll"      "%OUT%\unzipd32.dll"
+copy "..\steem\steem.new"             "%OUT%\steem.new"
+copy "%DOC_DIR%\DISK_IMG.PRG"         "%OUT%\DISK_IMG.PRG"
+copy "%DOC_DIR%\disk image howto.txt" "%OUT%\disk image howto.txt"
+copy "%DOC_DIR%\CART_IMG.PRG"         "%OUT%\CART_IMG.PRG"
+copy "%DOC_DIR%\cart image howto.txt" "%OUT%\cart image howto.txt"
+copy "%DOC_DIR%\faq.txt"              "%OUT%\faq.txt"
 
-copy "steemupdate\steemupdate.exe" "_DebugRelease\steemupdate.exe"
+copy "steemupdate\steemupdate.exe"    "%OUT%\steemupdate.exe"
 
-copy "%DOC_DIR%\debug.txt"            "_DebugRelease\debug.txt"
-copy "%DOC_DIR%\logsection.dat"       "_DebugRelease\logsection.dat"
+copy "%DOC_DIR%\debug.txt"            "%OUT%\debug.txt"
+copy "%DOC_DIR%\logsection.dat"       "%OUT%\logsection.dat"
